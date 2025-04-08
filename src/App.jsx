417979e00eeafe2dec87e7bc571f51adfcf72ce9
gardoffwhite <gardoffwhite@gmail.com>
2025-04-08 21:07:17 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AdminPage from './pages/AdminPage';
-import AdminLogin from './pages/AdminLogin';
-import RandomItemPage from './pages/RandomItemPage'; // ถ้ามีหน้า UI สุ่ม
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<RandomItemPage />} />
-        <Route path="/admin-login" element={<AdminLogin />} />
         <Route path="/admin" element={<AdminPage />} />
+        <Route path="/" element={<div>Welcome to N-age Warzone Frontend</div>} />
       </Routes>
     </Router>
   );
